857d8e013977e1db07bfdbb3bdbd67dca5500fd6
Felix Chan <felixchan29@gmail.com>
2020-02-12 21:24:39 -0500

    simple blog

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+    def about 
+        @title = "about us";
+        @content = "This is the about page"
+    end
+
+    def index
+        
+    end
+
+    def show
+
+    end
+end
+
