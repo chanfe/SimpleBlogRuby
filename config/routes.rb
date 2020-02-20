2a2e6f33153b8fa61b071cafa35ff03689ab5d84
Felix Chan <felixchan29@gmail.com>
2020-02-20 16:58:18 -0500

    fixed rails issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index', as: 'home'
-
-  get '/about/', to: 'pages#about', as: 'about'
-
-  resources :posts do 
-    resources :comments
-  end
 end
